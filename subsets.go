fa3ba0fb533085269fcd4714e66ef7c522a7b28d
lordtunk <stevvensa.550@gmail.com>
2013-12-17 22:16:26 -0500

    Changed subset generator to return a 2D slice

--- subsets.go
+++ subsets.go
@@ -3,33 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Starting subset generator\n")
+	out("Starting subset generator\n")
 	s := []string { "1", "2", "3" }
-	fmt.Printf("Generating 3 bit Gray codes\n")
 	gray := generateGrayCode(3)
-	prettyPrint(gray)
-	fmt.Printf("Generating all possible sub sets for: s = %s\n", s)
+	prettyPrint("gray", gray, true)
 	t := generateAllSubSets(s)
-	prettyPrint(t)
+	prettyPrint2D("all subsets", t)
 }
 
-func generateAllSubSets(s []string) ([]string) {
+func generateAllSubSets(s []string) ([][]string) {
 	if len(s) == 0 {
-		return s
+		return [][]string {}
 	}
+	// Generate an n-bit Gray code to determine subsets
 	gray := generateGrayCode(uint(len(s)))
-	subsets := make([]string, len(gray))
+	subsets := make([][]string, len(gray))
 	for i:=0; i<len(gray); i++ {
-		printed := false
-		for j:=0; j<len(gray[i]); j++ {
+		for j:=len(gray[i])-1; j>=0; j-- {
+			// Check if the bit is "1". If so, print the corresponding element 
 			if(string(gray[i][j]) == "1") {
 				ind := len(gray[i]) - j - 1
-				if(j > 0 && printed) {
-					subsets[i] = subsets[i] + "," + s[ind]
-				} else {
-					subsets[i] = subsets[i] + s[ind]
-				}
-				printed = true
+				subsets[i] = append(subsets[i], s[ind])
 			}
 		}
 	}
@@ -44,10 +38,10 @@ func generateGrayCode(n uint) (s []string) {
 	code[0], code[1] = "0", "1"
 
 	// Every iteration of this loop generates 2*i codes from previously
-    // generated i codes.
+	// generated i codes.
 	for i:=2; i<(1<<n); i = i<<1 {
 		// Enter the prviously generated codes again in arr[] in reverse
-        // order. Nor arr[] has double number of codes.
+		// order. Nor arr[] has double number of codes.
 		for j:=i-1; j>=0; j-- {
 			code = append(code, code[j])
 		}
@@ -63,14 +57,44 @@ func generateGrayCode(n uint) (s []string) {
 	return code
 }
 
-func prettyPrint(s []string) {
-	if(len(s) == 0) {
-		fmt.Printf("[ ]")
-	} else {
-		fmt.Printf("[ {%s}", s[0])
-		for i:=1;i<len(s);i++ {
-			fmt.Printf(", {%s}", s[i])
-		}
-		fmt.Printf(" ]\n")
+func prettyPrint(title string, s []string, useNewline bool) {
+	if title != "" {
+		out("%s = ", title)
 	}
+	if len(s) == 0 {
+		out("[ ]")
+		if useNewline {
+			out("\n")
+		}
+	} else {
+		out("[ %s", s[0])
+		for i:=1;i<len(s);i++ {
+			out(", %s", s[i])
+		}
+		out(" ]")
+		if useNewline {
+			out("\n")
+		}
+	}
+}
+
+func prettyPrint2D(title string,s [][]string) {
+	if title != "" {
+		out("%s = ", title)
+	}
+	if len(s) == 0 {
+		out("[ ]\n")
+	} else {
+		out("[ %s", s[0])
+		for i:=1;i<len(s);i++ {
+			out(", ")
+			prettyPrint("", s[i], false)
+		}
+		out(" ]\n")
+	}
+}
+
+// Shorthand function for fmt.Printf
+func out(format string, v...interface{}) {
+	fmt.Printf(format, v...)
 }
